0a8f41e70a9d27ee1b8b2678c152d0360cc243e8
marcus <marcusmmourao@gmail.com>
2019-06-29 18:12:47 -0300

    Start implement mm-input component

---
+++ src/components/text-input/mm-input.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label>
+      {{ label }}
+      <input
+        v-bind="$attrs"
+        v-on="listeners">
+    </label>
+    <span>{{ errorMessage }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mm-input',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    errorMessage: {
+      type: String,
+      default: null,
+    },
+  },
+  inheritAttrs: false,
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: event => this.$emit('input', event.target.value),
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "mm-input";
+</style>
