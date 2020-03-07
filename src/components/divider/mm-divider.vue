4542ec01ab5ec6ec2864dd39c7e90b90dc002560
marcus <marcusmmourao@gmail.com>
2020-03-07 17:04:54 -0300

    feat: implement divider component

---
+++ src/components/divider/mm-divider.vue
@@ -0,0 +1,28 @@
+<template>
+  <hr class="mm-divider" :class="classes" />
+</template>
+
+<script>
+export default {
+  name: 'mm-divider',
+  props: {
+    theme: {
+      type: String,
+      validator(theme) {
+        return ['dotted'].includes(theme);
+      },
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        'mm-divider--dotted': this.theme === 'dotted',
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "mm-divider";
+</style>
